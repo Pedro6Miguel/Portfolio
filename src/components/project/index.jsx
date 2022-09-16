a2fd905df99a038bb99d233014c5247043b357b3
Pedro Miguel <87210287+Pedro6Miguel@users.noreply.github.com>
2022-09-16 03:26:59 +0100

    Projects hover animation update

--- src/components/project/index.jsx
+++ src/components/project/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import styles from './styles.module.scss'
 
+import { AiFillGithub } from "react-icons/ai";
+import { FaSmileBeam } from "react-icons/fa";
+
 function Project(props) {
     return (
         <div className={styles.project}>
             <img src={props.image} alt="" />
             <div className={styles.hover}>
-                <a target="_blank" href="github.com"><div>VISIT WEBSITE</div></a>
-                <a target="_blank" href="github.com"><div>GITHUB</div></a>
+                <a target="_blank" href="github.com"><div>VISIT WEBSITE <FaSmileBeam /></div></a>
+                <a target="_blank" href="github.com"><div>GITHUB <AiFillGithub /></div></a>
             </div>
         </div>
     )
