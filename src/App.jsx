85cb206035f9d4d8020427e4782b13b1b0dd1b91
Pedro Miguel <87210287+Pedro6Miguel@users.noreply.github.com>
2022-09-20 01:57:45 +0100

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 
 import Header from './sections/Header'
+import Projects from './sections/Projects'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <Projects />
     </div>
   )
 }
---
+++ src/sections/Projects/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './styles.module.scss'
+
+function Projects() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.nav}>
+                NAVBAR
+            </div>
+            <div className={styles.content}>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi dolorem beatae, harum hic assumenda reprehenderit dignissimos pariatur eaque vero aliquam in nulla iure, sapiente similique quasi? Voluptatum nemo eligendi atque?
+            </div>
+        </div>
+    )
+}
+
+export default Projects
