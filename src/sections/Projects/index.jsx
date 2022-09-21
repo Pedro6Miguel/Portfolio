2fb2670c0487de0b45192f71ba91fc270677ffcf
Pedro Miguel <87210287+Pedro6Miguel@users.noreply.github.com>
2022-09-21 02:53:33 +0100

    Projects update

--- src/sections/Projects/index.jsx
+++ src/sections/Projects/index.jsx
@@ -18,8 +18,8 @@ function Projects() {
                         img="https://media.discordapp.net/attachments/905837522813337613/1021788071454322688/unknown.png?width=819&height=568"
                     />
                     <ProjectShowcase img="https://media.discordapp.net/attachments/905837522813337613/1021829996534317157/unknown.png?width=856&height=630" />
-                    <ProjectShowcase />
-                    <ProjectShowcase />
+                    <ProjectShowcase img="https://media.discordapp.net/attachments/905837522813337613/1021961299229409340/unknown.png?width=1228&height=630" />
+                    <ProjectShowcase img="https://media.discordapp.net/attachments/905837522813337613/1021944344422396005/unknown.png?width=1071&height=630" />
                     <ProjectShowcase />
                     <ProjectShowcase />
                     <ProjectShowcase />
