058889d37e6f2eb3e03a2ae707711969e8d6ed48
Pedro Miguel <87210287+Pedro6Miguel@users.noreply.github.com>
2022-10-27 02:23:41 +0100

    Form update

--- src/sections/Contact/index.jsx
+++ src/sections/Contact/index.jsx
@@ -14,7 +14,7 @@ function Contact() {
                         <input placeholder='Name' name='name' />
                         <input placeholder='Email' name='email' />
                         <textarea placeholder='Message' name='message' />
-                        <input type="hidden" name="_next" value="http://127.0.0.1:5173/"></input>
+                        <input type="hidden" name="_next" value="https://pedromigueldev.vercel.app/"></input>
                         <button type='submit'>Send</button>
                     </form>
                     <div>
